bef15a039a64dbf7cfdb3692dffdd009092c19a8
Subramaniam Jeyaseelan <jeyaseelan421@gmail.com>
2022-03-27 23:56:06 +0530

    asynchronous example1

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-//synchronous
+// //synchronous
+// //javascript runs top to bottom
+// console.log("I ");
+// console.log("eat ");
+// console.log("spoon ");
+// console.log("Ice cream ");
+// console.log("with ");
 
+//Asynchrnouns
 console.log("I ");
 console.log("eat ");
-console.log("Ice cream ");
+setTimeout(()=>{
+    console.log("Ice cream ");
+},4000)  // this is asynchronous call, separated somewhere and released after 4 seconds
 console.log("with ");
 console.log("spoon ");
+
+
+
+
