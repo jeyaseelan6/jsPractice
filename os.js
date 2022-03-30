94d60dca4db1d4c2d2cc7fa1113a610d5ff167d4
Subramaniam Jeyaseelan <jeyaseelan421@gmail.com>
2022-03-30 17:33:59 +0530

    Mosh tutorial fs functions

--- os.js
+++ os.js
@@ -5,3 +5,10 @@ var freeMemory = os.freemem();
 
 console.log("Total Memory is " + totalMemory);
 console.log("Free Memory is " + freeMemory);
+
+// ithu normal concadinations java la padicathu pola
+// ana js la ES6 la Template string endu solli ondu iruku
+// `${totalMemory} is available
+
+console.log(`Total Memory is ${totalMemory}`);
+console.log(`Free Memory is ${freeMemory}`);
